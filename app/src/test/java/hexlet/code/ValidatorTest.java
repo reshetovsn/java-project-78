1adf3114960390535a78bc9ac54d0555f85ca58d
Sergey Reshetov <reshetov.sn@gmail.com>
2023-05-06 17:26:42 +0300

    modified ValidatorTest

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,8 +1,13 @@
 package hexlet.code;
 
+import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ValidatorTest {
@@ -14,11 +19,11 @@ class ValidatorTest {
 
         assertThat(schema.isValid("")).isTrue();
         assertThat(schema.isValid(null)).isTrue();
+        assertThat(schema.isValid(5)).isFalse();
 
         schema.required();
         assertThat(schema.isValid("")).isFalse();
         assertThat(schema.isValid(null)).isFalse();
-        assertThat(schema.isValid(5)).isFalse();
         assertThat(schema.isValid("some text for check")).isTrue();
 
         schema.minLength(3);
@@ -27,6 +32,7 @@ class ValidatorTest {
 
         assertThat(schema.contains("some").isValid("some text for check")).isTrue();
         assertThat(schema.contains("sometext").isValid("some text for check")).isFalse();
+
         assertThat(schema.isValid("some text for check")).isFalse();
     }
 
@@ -38,11 +44,11 @@ class ValidatorTest {
         assertThat(schema.isValid(null)).isTrue();
         assertThat(schema.isValid("5")).isFalse();
         assertThat(schema.isValid(-10)).isTrue();
+
         assertThat(schema.positive().isValid(null)).isTrue();
 
         schema.required();
         assertThat(schema.isValid(null)).isFalse();
-        assertThat(schema.isValid("5")).isFalse();
         assertThat(schema.isValid(-10)).isFalse();
         assertThat(schema.isValid(0)).isFalse();
 
@@ -51,5 +57,26 @@ class ValidatorTest {
         assertThat(schema.isValid(10)).isTrue();
         assertThat(schema.isValid(4)).isFalse();
         assertThat(schema.isValid(11)).isFalse();
+    }
+
+    @Test
+    public void testMap() {
+        Validator v = new Validator();
+        MapSchema schema = v.map();
+
+        assertThat(schema.isValid(null)).isTrue();
+        assertThat(schema.isValid("5")).isFalse();
+        assertThat(schema.isValid(Map.of("key", "data"))).isTrue();
+
+        schema.required();
+        assertThat(schema.isValid(null)).isFalse();
+        assertThat(schema.isValid(new HashMap<>())).isTrue();
+
+        Map<String, String> data = new HashMap<>();
+        data.put("key1", "value1");
+        schema.sizeof(2);
+        assertThat(schema.isValid(data)).isFalse();
+        data.put("key2", "value2");
+        assertThat(schema.isValid(data)).isTrue();
     }
 }
