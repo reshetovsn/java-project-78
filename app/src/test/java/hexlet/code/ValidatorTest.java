341418b4e276dffc714ee995b77975837780ef64
Sergey Reshetov <reshetov.sn@gmail.com>
2023-05-10 13:59:13 +0300

    Modified ValidatorTest.java

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,6 +1,6 @@
 package hexlet.code;
 
-//import hexlet.code.schemas.BaseSchema;
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
@@ -84,34 +84,34 @@ class ValidatorTest {
         assertThat(schema.isValid(data)).isFalse();
     }
 
-//    @Test
-//    public void testShape() {
-//        Validator v = new Validator();
-//        MapSchema schema = v.map();
-//        Map<String, BaseSchema> schemas = new HashMap<>();
-//
-//        schemas.put("name", v.string().required());
-//        schemas.put("age", v.number().positive());
-//        schema.shape(schemas);
-//
-//        Map<String, Object> human1 = new HashMap<>();
-//        human1.put("name", "Kolya");
-//        human1.put("age", 100);
-//        assertThat(schema.isValid(human1)).isTrue();
-//
-//        Map<String, Object> human2 = new HashMap<>();
-//        human2.put("name", "Maya");
-//        human2.put("age", null);
-//        assertThat(schema.isValid(human2)).isTrue();
-//
-//        Map<String, Object> human3 = new HashMap<>();
-//        human3.put("name", "");
-//        human3.put("age", null);
-//        assertThat(schema.isValid(human3)).isFalse();
-//
-//        Map<String, Object> human4 = new HashMap<>();
-//        human4.put("name", "Valya");
-//        human4.put("age", -5);
-//        assertThat(schema.isValid(human4)).isFalse();
-//    }
+    @Test
+    public void testShape() {
+        Validator v = new Validator();
+        MapSchema schema = v.map();
+        Map<String, BaseSchema> schemas = new HashMap<>();
+
+        schemas.put("name", v.string().required());
+        schemas.put("age", v.number().positive());
+        schema.shape(schemas);
+
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Kolya");
+        human1.put("age", 100);
+        assertThat(schema.isValid(human1)).isTrue();
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        assertThat(schema.isValid(human2)).isTrue();
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        assertThat(schema.isValid(human3)).isFalse();
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Valya");
+        human4.put("age", -5);
+        assertThat(schema.isValid(human4)).isFalse();
+    }
 }
